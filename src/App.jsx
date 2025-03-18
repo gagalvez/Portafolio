07e6bef00d5c9384c8103a9f966ba0f4b07e86ce
gagalvez <gagalvezv@gmail.com>
2025-03-17 23:34:39 -0300

    Primeros pasos en portafolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sidebar from "./components/SideBar";
+import "./css/App.css";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,25 @@
+import "../css/SideBar.css";
+
+function Sidebar() {
+  return (
+    <div className="side-bar">
+      <div className="side-bar-photo">
+        <img src="#" alt="#" />
+      </div>
+      <div className="side-bar-info">
+        <h1>Gabriel Galvez Valenzuela</h1>
+        <p>Analista Programador en formacion</p>
+      </div>
+      <div className="side-bar-nav">
+        <ul>
+          <li>asd</li>
+          <li>213</li>
+          <li>tasd</li>
+          <li>224</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
